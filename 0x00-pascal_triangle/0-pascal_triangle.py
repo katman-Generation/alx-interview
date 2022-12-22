623d9f4c5c77d3cf9907d4d6907c429367bb13c9
katman-Generation <tkatema5@gmail.com>
2022-12-22 22:15:14 +0200

    submitting

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+pascal triangle
+"""
+def pascal_triangle(n):
+    """
+    returns a list of intergers 
+    of a pascal triangle.
+    """
+    main_List = []
+    if n <= 0:
+        return []
+    
+    for t in range(n):
+        temp_List = []
+        for k in range(t+1):
+            if t==0 or k==t or k==0:
+                temp_List.append(1)
+            else:
+                temp_List.append(main_List[t-1][k-1] + main_List[t-1][k])
+    main_List.append(temp_List)
+
+    return main_List
