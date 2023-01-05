e9afd0d5b056bb3fc5ec3da1c635251cdc43229a
katman-Generation <tkatema5@gmail.com>
2023-01-05 12:07:30 +0200

    submitting

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,13 +6,13 @@ from 0 to n - 1 and each box may
 contain keys to the other boxes.
 """
 
-
 def canUnlockAll(boxes):
     """
      a method that determines if all the boxes can be opened.
     :param boxes:
     :return: True or False
     """
+    
     if not boxes or type(boxes) is not list:
         return False
 
